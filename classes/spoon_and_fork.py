892c7c4d6df81584ca994c647c0da9c1cf5a9f27
slyvka800 <74894902+slyvka800@users.noreply.github.com>
2021-03-23 21:37:48 +0200

    Update spoon_and_fork.py

--- classes/spoon_and_fork.py
+++ classes/spoon_and_fork.py
@@ -8,8 +8,8 @@ class SpoonType(Enum):
 
 
 class Spoon(cutlery.Cutlery):
-    def __init__(self, amount_of_pieces, origin_country: cutlery.father.Country, type_of_dishes: cutlery.father.Dishes,
-                 material: cutlery.father.Material, price, brand, thickness: cutlery.father.Thickness,
+    def __init__(self, amount_of_pieces, origin_country: cutlery.Country, type_of_dishes: cutlery.Dishes,
+                 material: cutlery.Material, price, brand, thickness: cutlery.Thickness,
                  date_of_manufacture: list, length_in_inches, spoon_type: SpoonType):
         super().__init__(amount_of_pieces, origin_country, type_of_dishes, material, price, brand, thickness,
                          date_of_manufacture, length_in_inches)
@@ -23,8 +23,8 @@ class Spoon(cutlery.Cutlery):
 
 
 class Fork(cutlery.Cutlery):
-    def __init__(self, amount_of_pieces, origin_country: cutlery.father.Country, type_of_dishes: cutlery.father.Dishes,
-                 material: cutlery.father.Material, price, brand, thickness: cutlery.father.Thickness,
+    def __init__(self, amount_of_pieces, origin_country: cutlery.Country, type_of_dishes: cutlery.Dishes,
+                 material: cutlery.Material, price, brand, thickness: cutlery.Thickness,
                  date_of_manufacture: list, length_in_inches, prong_amount):
         super().__init__(amount_of_pieces, origin_country, type_of_dishes, material, price, brand, thickness,
                          date_of_manufacture, length_in_inches)
@@ -35,9 +35,3 @@ class Fork(cutlery.Cutlery):
         self._output = 'It is a fork\n' + super(Fork, self).__str__() + '\n'
         self._output += f'prong amount: {self._prong_amount}\n'
         return self._output
-
-
-#a = Spoon(12, cutlery.father.Country.Ukraine, cutlery.father.Dishes.Spoons, cutlery.father.Material.Paper, 123,
-#          "Nice Tableware", cutlery.father.Thickness.Low, [12, 28, 2002], 1240, SpoonType.TableSpoon)
-#print(Spoon.__mro__)
-#print(a.__str__())
