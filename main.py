bbddd61d410cb9473cc5ab36f7695d8d9254a048
slyvka800 <74894902+slyvka800@users.noreply.github.com>
2021-04-04 17:58:06 +0300

    Improving my code

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import os
 
 def main():
     log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "access_log")
-    regex = '\[(?:0[8-9]|10)\/Mar\/2004:(?:0[7-9]|10|11):(?:(?!10)[1-3][0-9]|40|41)' \
-            ':(?:3[7-9]|4\d|5[0-2]) -0800] ".*" 200'
+    regex = r'\[(?:0[8-9]|10)\/Mar\/2004' \
+            r':(?:0[7-9]|10|11)' \
+            r':(?:(?!10)[1-3][0-9]|40|41)' \
+            r':(?:3[7-9]|4\d|5[0-2])' \
+            r' -0800] ".*" 200'
     counter = 0
 
     with open(log_file_path, "r") as file:
