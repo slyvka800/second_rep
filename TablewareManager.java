b98a2aafa7eb5a86dccde23571131dc32c51071c
slyvka800 <74894902+slyvka800@users.noreply.github.com>
2021-05-06 09:50:37 +0300

    Update TablewareManager.java

--- TablewareManager.java
+++ TablewareManager.java
@@ -2,71 +2,44 @@ package iot.ua;
 
 import lombok.Builder;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Builder
-public class TablewareManager {
-    private List<PackageOfDisposableTableware> items = new ArrayList<>();
+public class TablewareManager{
+    private final List<PackageOfDisposableTableware> items;
 
     public List<PackageOfDisposableTableware> searchByMaterial(Material material)
     {
         List<PackageOfDisposableTableware> arrayOfFounds = new ArrayList<>();
         for(PackageOfDisposableTableware item: items) {
-            if(item.getMaterial() == material) {
+            if (item.getMaterial() == material) {
                 arrayOfFounds.add(item);
             }
         }
         return arrayOfFounds;
     }
 
-    public void sortByPrice(List<PackageOfDisposableTableware> items, SortOrder order)
+    public void sortByPrice(List<PackageOfDisposableTableware> itemsForSort, SortOrder order)
     {
         if(order == SortOrder.ASC) {
-            items.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second)->
+            itemsForSort.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second)->
                     (int)(first.getPrice() - second.getPrice()));
         } else {
-            items.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second)->
+            itemsForSort.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second)->
                     (int)(second.getPrice() - first.getPrice()));
         }
     }
 
-    public void sortByDate(List<PackageOfDisposableTableware> items, SortOrder order)
-    {
-        items.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second) -> {
-            int[] firstInt = first.getDateOfManufacture();
-            int[] secondInt = second.getDateOfManufacture();
-            StringBuilder firstStrBuilder = new StringBuilder();
-            StringBuilder secondStrBuilder = new StringBuilder();
+    public void sortByDate(List<PackageOfDisposableTableware> itemsForSort, SortOrder order) {
+        if(order == SortOrder.ASC)
+        {
+            itemsForSort.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second) ->
+                    first.getDateOfManufacture().compareTo(second.getDateOfManufacture()));
+        } else {
+            itemsForSort.sort((PackageOfDisposableTableware first, PackageOfDisposableTableware second) ->
+                    second.getDateOfManufacture().compareTo(first.getDateOfManufacture()));
+        }
 
-            firstStrBuilder.append(firstInt[0]).append("-").append(firstInt[1]).append("-").append(firstInt[2]);
-            secondStrBuilder.append(secondInt[0]).append("-").append(secondInt[1]).append("-").append(secondInt[2]);
-
-            String firstStr = firstStrBuilder.toString();
-            String secondStr = secondStrBuilder.toString();
-
-            SimpleDateFormat objSDF = new SimpleDateFormat("dd-MM-yyyy");
-            try {
-                Date dt_1 = objSDF.parse(firstStr);
-
-                try {
-                    Date dt_2 = objSDF.parse(secondStr);
-                    if(order == SortOrder.ASC) {
-                        return dt_1.compareTo(dt_2);
-                    } else {
-                        return dt_2.compareTo(dt_1);
-                    }
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            return 0;
-        });
     }
 }
