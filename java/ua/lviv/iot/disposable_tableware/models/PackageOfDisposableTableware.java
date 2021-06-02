b85a9961355bf02046ef69076bf9f143757b021e
slyvka800 <74894902+slyvka800@users.noreply.github.com>
2021-06-02 17:34:52 +0300

    Adding files of lab 9

--- java/ua/lviv/iot/disposable_tableware/models/PackageOfDisposableTableware.java
+++ java/ua/lviv/iot/disposable_tableware/models/PackageOfDisposableTableware.java
@@ -7,18 +7,11 @@ package ua.lviv.iot.disposable_tableware.models;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.*;
 import java.util.Date;
 
 @SuperBuilder
 @Data
-@MappedSuperclass
 public abstract class PackageOfDisposableTableware {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
     private int amountOfPieces;
     private Country originCountry;
     private Dishes type;
