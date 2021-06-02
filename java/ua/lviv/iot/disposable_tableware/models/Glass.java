103a16bb786f94f828cb62345f898090c97221e2
slyvka800 <74894902+slyvka800@users.noreply.github.com>
2021-06-02 17:44:00 +0300

    Lab 10 (second try)

--- java/ua/lviv/iot/disposable_tableware/models/Glass.java
+++ java/ua/lviv/iot/disposable_tableware/models/Glass.java
@@ -2,13 +2,23 @@ package ua.lviv.iot.disposable_tableware.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @SuperBuilder
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Glass extends Drinkware {
-    private int id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
 
     private String color;
     private String decoration;
--- java/ua/lviv/iot/disposable_tableware/service/GlassService.java
+++ java/ua/lviv/iot/disposable_tableware/service/GlassService.java
@@ -1,48 +1,49 @@
 package ua.lviv.iot.disposable_tableware.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
+import ua.lviv.iot.disposable_tableware.dal.GlassRepository;
 import ua.lviv.iot.disposable_tableware.models.Glass;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.management.InstanceNotFoundException;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Service
 @ApplicationScope
 public class GlassService {
 
-    private AtomicInteger id = new AtomicInteger(0);
-
-    private Map<Integer, Glass> glassMap = new HashMap<Integer, Glass>();
+    @Autowired
+    private GlassRepository repository;
 
     public Glass addGlass(Glass glass) {
-        Integer glassId = id.incrementAndGet();
-        glass.setId(glassId);
-        glassMap.put(glassId, glass);
-        return glass;
+        return repository.save(glass);
     }
 
     public List<Glass> getGlasses() {
-        return new ArrayList<>(glassMap.values());
+        return repository.findAll();
     }
 
     public Glass getGlass(Integer id) {
-        return glassMap.get(id);
+        return repository.findById(id).orElseThrow();
     }
 
-    public Glass updateGlass(Glass glass) {
-        return glassMap.put(glass.getId(), glass);
+    public Glass updateGlass(Glass glass) throws InstanceNotFoundException{
+        var oldGlass = repository.findById(glass.getId()).orElse(null);
+        if(oldGlass == null) {
+            throw new InstanceNotFoundException("You are trying to update non-existing glass");
+        }
+        repository.save(glass);
+        return oldGlass;
     }
 
-    public Glass updateGlass(Integer id, Glass glass) {
-        return glassMap.put(id, glass);
-    }
-
-    public Glass deleteGlass(Integer id) {
-        return glassMap.remove(id);
+    public Glass deleteGlass(Integer id) throws InstanceNotFoundException{
+        var deletingObject = repository.findById(id).orElse(null);
+        if(deletingObject == null) {
+            throw new InstanceNotFoundException("You are trying to delete non-existing glass");
+        }
+        repository.deleteById(id);
+        return deletingObject;
     }
 }
